aec42ced45b5e9b82cebd2f21ea9f947ec2c5aba
Shane Dupree <shane.dupree@gmail.com>
2022-02-14 14:32:30 -0500

    Update homepage button position.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header({user, setUser}) {
 
   return (
     <header>
-      <Link to="/"><img src="/images/wkb1emp_header_logo-transparent-161px.png" alt="Logo"/></Link>
+      <Link className="homeLink" to="/"><img src="/images/wkb1emp_header_logo-transparent-161px.png" alt="Logo"/></Link>
       &nbsp; | &nbsp;
       Now Playing Component
       &nbsp; | &nbsp;
